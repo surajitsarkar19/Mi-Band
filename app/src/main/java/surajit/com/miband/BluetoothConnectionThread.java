f87de4274a21db53a7c697933c517f5206fc0fb7
Surajit Sarkar <surajit@bitcanny.com>
2017-02-09 19:21:55 +0530

    minor changes

--- app/src/main/java/surajit/com/miband/BluetoothConnectionThread.java
+++ app/src/main/java/surajit/com/miband/BluetoothConnectionThread.java
@@ -1,10 +1,10 @@
 package surajit.com.miband;
 
 /**
- * Created by Surajit Sarkar on 8/2/17.
+ * Created by Surajit Sarkar on 9/2/17.
  * Company : Bitcanny Technologies Pvt. Ltd.
  * Email   : surajit@bitcanny.com
  */
 
-public class BluetoothClientThread {
+public class BluetoothConnectionThread {
 }
